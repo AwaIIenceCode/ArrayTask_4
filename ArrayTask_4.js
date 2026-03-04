4a1fc25bcec90676a2f40fbb5511ff8826e74a12
AwallenceCode <awallencecode@gmail.com>
2026-03-04 23:48:32 +0200

    Add placeholder functions for array operations: createArray, printArray, myMax, and myMin

--- ArrayTask_4.js
+++ ArrayTask_4.js
@@ -1,3 +1,15 @@
+// function for generation array
+function createArray()
+{
+
+}
+
+// function for print array
+function printArray()
+{
+
+}
+
 //
 function myMap()
 {
@@ -19,10 +31,22 @@ function myReduce()
 //
 function myBubbleSort()
 {
-    
+
 }
 
-//
+// function for search max number in an array
+function myMax()
+{
+
+}
+
+// function for search min number in an array
+function myMin()
+{
+
+}
+
+// main function
 function main()
 {
 
